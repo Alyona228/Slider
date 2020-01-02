6d747e21693b82196ec5c052a6b5b1ee940b1a1c
Name <56693963+Alyona228@users.noreply.github.com>
2020-01-02 17:05:59 +0300

    Init

---
+++ script.js
@@ -0,0 +1,60 @@
+const data = [{
+    img: 'url("./images/image2.1.jpg")',
+    city: 'Rostov-on-Don LCD admiral',
+    area: '81 m2',
+    time: '3.5 months',
+    cost: 'Upon request',
+  },
+  {
+    img: 'url("./images/image3.jpg")',
+    city: 'Sochi Thieves',
+    area: '105 m2',
+    time: '4 months',
+    cost: 'Upon request',
+  },
+  {
+    img: 'url("./images/image2.jpg")',
+    city: 'Rostov-on-Don Patriotic',
+    area: '93 m2',
+    time: '3 months',
+    cost: 'Upon request',
+  },
+]
+
+let currentIndex = 0
+
+function setCurrentParams(index) {
+  const img = document.querySelector('.page2__images')
+  const city = document.getElementById('city')
+  const area = document.getElementById('area')
+  const time = document.getElementById('time')
+  const cost = document.getElementById('cost')
+
+  img.style.backgroundImage = data[index].img
+  area.textContent = data[index].area
+  time.textContent = data[index].time
+  cost.textContent = data[index].cost
+  city.textContent = data[index].city
+
+  currentIndex = index
+}
+
+const arrowLeft = document.querySelector('.page2__arrow--left')
+arrowLeft.addEventListener('mousedown', e => {
+  if (currentIndex < data.length - 1) {
+    setCurrentParams(currentIndex + 1)
+  } else {
+    setCurrentParams(0)
+  }
+})
+
+
+
+const arrowRight = document.querySelector('.page2__arrow--right')
+arrowRight.addEventListener('mousedown', e => {
+  if (currentIndex == 0) {
+    setCurrentParams(data.length - 1)
+  } else {
+    setCurrentParams(currentIndex - 1)
+  }
+})
